35fec0388c60c97329ae16c5aa8d4af4f411e771
rkeenan <ross.keenan@protonmail.comm>
2024-04-03 10:15:52 -0500

    added input binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import Fun from './Components/Fun.svelte';
-  let name = 'Bob';
   let favoriteColor = 'red';
+  let name = "";
+  let isDisabled = false;
 </script>
 
 <div>
   <h1>Hello there, <span style="color: {favoriteColor}">{name}!</span></h1>
   <div>
     <Fun fontSize={40} />
+    <input type="text" placeholder="Enter your name" bind:value={name}/>
+    <button disabled={isDisabled}>Click Me</button>
   </div>
 </div>
